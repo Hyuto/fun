3ebc430148368261a05f14a46a9b01cb74964fc1
Wahyu Setianto <wahyusetianto19@gmail.com>
2021-07-19 23:50:49 +0700

    Try pywebview using flask and todo react project

---
+++ GUI/test-pyweb/main.py
@@ -0,0 +1,17 @@
+import webview
+from flask import Flask, send_from_directory
+
+DEBUG = False
+
+server = Flask("__main__",
+               template_folder='frontend',
+               static_folder='frontend/static')
+
+
+@server.route('/')
+def serve():
+    return send_from_directory(server.template_folder, 'index.html')
+
+
+window = webview.create_window('test-pyweb', server)
+webview.start(debug=DEBUG)
